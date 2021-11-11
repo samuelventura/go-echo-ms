d683402f5a42c800347cd87a534d2d805c4de37b
Samuel Ventura <samuel.ventura@yeico.com>
2021-11-10 21:52:54 -0600

    Parametric installer

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
 #!/bin/bash -x
 
 if [[ "$OSTYPE" == "linux"* ]]; then
-    BIN=go-echo-ms
+    DAEMON=echo
+    BINARY=go-echo-ms
     SRC=$HOME/go/bin
     DST=/usr/local/bin
     go install
-    DAEMON=echo
-    curl -X POST http://127.0.0.1:31600/api/daemon/stop/echo
-    curl -X POST http://127.0.0.1:31600/api/daemon/uninstall/echo
+    curl -X POST http://127.0.0.1:31600/api/daemon/stop/$DAEMON
+    curl -X POST http://127.0.0.1:31600/api/daemon/uninstall/$DAEMON
     sudo cp $SRC/$BIN $DST
-    curl -X POST http://127.0.0.1:31600/api/daemon/install/echo?path=$DST/$BIN
-    curl -X POST http://127.0.0.1:31600/api/daemon/enable/echo
-    curl -X POST http://127.0.0.1:31600/api/daemon/start/echo
-    curl -X GET http://127.0.0.1:31600/api/daemon/info/echo
-    curl -X GET http://127.0.0.1:31600/api/daemon/env/echo
+    curl -X POST http://127.0.0.1:31600/api/daemon/install/$DAEMON?path=$DST/$BINARY
+    curl -X POST http://127.0.0.1:31600/api/daemon/enable/$DAEMON
+    curl -X POST http://127.0.0.1:31600/api/daemon/start/$DAEMON
+    curl -X GET http://127.0.0.1:31600/api/daemon/info/$DAEMON
+    curl -X GET http://127.0.0.1:31600/api/daemon/env/$DAEMON
 fi
